45928d97b8db97b1d7f571650401d5005b4797ef
Paolo Carcamo <Pogi@Paolo-Carcamo.local>
2021-01-30 21:40:55 -0800

    Actually added error message functionality

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/* Along the same lines as the use of inputs and logic in this program, I can build my program for
+spitting out the acitivities I need to make for each event that I'm assigned. */
+
 // Listen for submit button
 document
   .getElementById('loan-form')
@@ -30,11 +33,36 @@ function calculateResults(e) {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
-    console.log('Please check your numbers');
+    // Named funciton for showing the error in case inputs have incorrect values
+    showError('Please check your numbers');
   }
 
   e.preventDefault();
 }
 
-/* Along the same lines as the use of inputs and logic in this program, I can build my program for
-spitting out the acitivities I need to make for each event that I'm assigned. */
+// Show Error Function
+function showError(error) {
+  // Create div for the error
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear Error Function
+function clearError() {
+  document.querySelector('.alert').remove();
+}
